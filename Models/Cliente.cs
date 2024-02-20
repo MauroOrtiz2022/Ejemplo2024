283a46eed542a0a3ff738144f09a6a0549c1bbba
PC01-1H <PC01-1H@PC21-1H>
2024-02-20 15:26:09 -0600

    Agregar archivos de proyecto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ejemplo.Models
+{
+    [Table("Clientes")]//Se define el nombre de la tabla que se conectara
+    public class Cliente
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Esta linea va seguida de la llave primaria
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Mesa { get; set; }
+    }
+}
---
+++ Models/Orden.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejemplo.Models
+{
+    [Table("Ordenes")]//Se define el nombre de la tabla que se conectara
+    public class Orden
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Esta linea va seguida de la llave primaria
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Numero { get; set; }
+        public string Foto { get; set; } = "";
+        public decimal Costo { get; set; }
+
+        [DataType(dataType.date)]
+        public DateTime Fecha { get; set; }
+        public virtual Cliente Cliente { get; set; }
+    }
+}
