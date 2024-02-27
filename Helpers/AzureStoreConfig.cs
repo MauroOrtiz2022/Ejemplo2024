3ba71b295817c92405b0b393660173c6e991a987
MauroOrtiz2022 <omauro193@gmail.com>
2024-02-27 15:53:20 -0600

    MARTES

---
+++ Helpers/AzureStoreConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejemplo.Helpers
+{
+    public class AzureStoreConfig
+    {
+        public string Cuenta { get; set; }
+
+        public string Llave { get; set; }
+
+        public string Contenedor { get; set; }
+    }
+}
